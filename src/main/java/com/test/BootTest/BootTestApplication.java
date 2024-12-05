f2261a877ff7dc457c402f09e443155c1a2a9a7a
YOUNGGYUJUNG <wjddudrb190@naver.com>
2024-12-05 10:04:27 +0900

    new fileasd

--- src/main/java/com/test/BootTest/BootTestApplication.java
+++ src/main/java/com/test/BootTest/BootTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootTestApplication.class, args);
+
 	}
 
 }
